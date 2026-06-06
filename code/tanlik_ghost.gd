27cb85afaf4696ffc5ba7a6c6f56e6540b09f801
JAIMEPCI <jaimeolarte888@gmail.com>
2026-06-06 13:34:24 -0500

    dialogue bubble implemented

--- code/tanlik_ghost.gd
+++ code/tanlik_ghost.gd
@@ -2,11 +2,14 @@ extends Area2D
 
 @onready var exclamation_mark = $ExclamationMark
 
+const MY_DIALOGUE = preload("res://Dialogue/Scene1/Dialogue1.dialogue")
+
+
 var is_player_close = false
 
 func _process(delta):
 	if is_player_close and Input.is_action_just_pressed("ui_accept"):
-		print("Iniciar dialogo")
+		DialogueManager.show_dialogue_balloon(MY_DIALOGUE)
 
 func _on_area_entered(area):
 	exclamation_mark.visible = true
