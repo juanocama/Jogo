e3863879c18fd9afc8222f31e1244f7ce5abeda6
JAIMEPCI <jaimeolarte888@gmail.com>
2026-06-06 17:27:57 -0500

    Dialogue font change and spam ignored

--- code/tanlik_ghost.gd
+++ code/tanlik_ghost.gd
@@ -4,11 +4,16 @@ extends Area2D
 
 const MY_DIALOGUE = preload("res://Dialogue/Scene1/Dialogue1.dialogue")
 
-
+var is_dialogue_active = false
 var is_player_close = false
 
+func _ready():
+	DialogueManager.dialogue_started.connect(_on_dialogue_started)
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
+	
+
 func _process(delta):
-	if is_player_close and Input.is_action_just_pressed("ui_accept"):
+	if is_player_close and Input.is_action_just_pressed("ui_accept") and not is_dialogue_active:
 		DialogueManager.show_dialogue_balloon(MY_DIALOGUE)
 
 func _on_area_entered(area):
@@ -18,3 +23,11 @@ func _on_area_entered(area):
 func _on_area_exited(area):
 	exclamation_mark.visible = false
 	is_player_close = false	
+	
+func _on_dialogue_started(dialogue):
+	is_dialogue_active = true
+
+func _on_dialogue_ended(dialogue):
+	is_dialogue_active = false
+		
+	
