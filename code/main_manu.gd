36b12ba64432f31e3825da4491df0a86b5a85d83
JAIMEPCI <jaimeolarte888@gmail.com>
2026-06-06 22:46:24 -0500

    Tittle Card Done

--- code/main_manu.gd
+++ code/main_manu.gd
@@ -11,10 +11,6 @@ func _on_start_pressed():
 	get_tree().change_scene_to_file("res://scenes/dialogue_test.tscn")
 
 
-func _on_options_pressed():
-	pass # Replace with function body.
-
-
 func _on_exit_pressed():
 	get_tree().quit()
 	
